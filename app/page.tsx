ff3c0168ff597e8a670eec7de5a81689cb44fb88
Bryan <bryan.cash78@gmail.com>
2024-10-17 18:44:24 -0400

    initial convex overview

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 
+import { api } from "@/convex/_generated/api";
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
+
+    const documents = useQuery(api.documents.getDocuments);
+    const createDocument = useMutation(api.documents.createDocument);
+
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -13,6 +18,13 @@ export default function Home() {
                 </Unauthenticated>
                 <Authenticated>
                     <UserButton />
+
+                    <button onClick={() => {
+                        createDocument({ title: 'Hello World' })
+                    }}>Click Me</button>
+                    {documents?.map((doc) => (
+                        <div key={doc._id}>{doc.title}</div>
+                    ))}
                 </Authenticated>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
